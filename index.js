5fd10962e54c6408aacc725ff44f5a4929bf5965
Lourenço Morejón <65128813+LourencoMorejon97@users.noreply.github.com>
2022-03-16 12:50:06 +0000

    first commit of repo

---
+++ index.js
@@ -0,0 +1,12 @@
+const chalk = require("chalk");
+
+console.log(chalk.blue("Alexandre"));
+console.log(chalk.red("Francisco"));
+console.log(chalk.green("Nuno"));
+console.log(chalk.black("Tiago"));
+
+//Comentário//
+
+
+
+
